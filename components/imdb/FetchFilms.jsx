3ac9bef40c387eac87cc9baa779d19c149a44f31
IppolitovIS <IppolitovIS@gmail.com>
2023-09-11 09:36:33 +0300

    Button fixed

--- components/imdb/FetchFilms.jsx
+++ components/imdb/FetchFilms.jsx
@@ -1,24 +1,7 @@
 import { useState } from "react"
 import OneFilm from "./OneFilm"
-import styles from "./OneFilm.module.css"
 
-export default function FetchFilms({ search }) {
-    const
-        [films, setFilms] = useState(null),
-        [error, setError] = useState(null),
-        onClick = async () => {
-            async function f() {
-                try {
-                    const
-                        response = await fetch("https://www.omdbapi.com/?apikey=a2b07930&s=" + search);
-                    if (!response.ok) throw new Error('fetch ' + response.status);
-                    setFilms(await response.json());
-                } catch (err) {
-                    setError(err);
-                }
-            }; 
-            f();
-        };
+export default function FetchFilms({ films, error }) {
 
     if (error) return <>Error = {error}</>
     if (films) {
@@ -29,5 +12,5 @@ export default function FetchFilms({ search }) {
         return <>{Search.map((film) => <OneFilm  key={film.imdbID} film={film} />)}Поиск</>
     }
 
-    return <><button className={styles.btn} onClick={onClick}>Поиск</button></>
+    return <></>
 }
--- components/imdb/index.jsx
+++ components/imdb/index.jsx
@@ -4,12 +4,28 @@ import styles from "./OneFilm.module.css"
 
 export default function FilmSearch({}) {
     const
-        [value, setValue] = useState() 
+        [films, setFilms] = useState(null),
+        [error, setError] = useState(null),
+        [value, setValue] = useState(),
+        onClick = async () => {
+            async function f() {
+                try {
+                    const
+                        response = await fetch("https://www.omdbapi.com/?apikey=a2b07930&s=" + value);
+                    if (!response.ok) throw new Error('fetch ' + response.status);
+                    setFilms(await response.json());
+                } catch (err) {
+                    setError(err);
+                }
+            }; 
+            f();
+        };
     return (
         <>
             <div className={styles.cent}>
                 <input value={value} className={styles.inp} onInput={evt => setValue(evt.target.value)} />
-                <FetchFilms search={value} />
+                <button className={styles.btn} onClick={onClick}>Поиск</button>
+                <FetchFilms films={films} error={error} />
             </div>
         </>
     )
